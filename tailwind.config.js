c855a9d735bbcf4ac13647ab29c46b4960c1ea71
Titouan Luciani <titouan.luciani98@gmail.com>
2021-02-04 14:34:52 +0100

    @tailwind globals.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    minWidth: {
+      "400": "400px"
+    },
+    maxWidth: {
+      "100":"100vw",
+    },
   },
   variants: {
     extend: {},
