2a94371c8a65bb6ac0b576bfce0e028b401de331
Titouan Luciani <titouan.luciani98@gmail.com>
2021-02-05 15:38:35 +0100

    regex cookie

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import {useEffect, useState} from 'react'
 function MyApp({ Component, pageProps }) {
   const [cookie, setCookie] = useState('')
   useEffect(() => {
-    setCookie(document.cookie.split(";").find(row=>row.startsWith(' userUrl')).split('=')[1])
+    setCookie(document.cookie.split(";").find(row=>row.startsWith('userUrl')).split('=')[1])
     //const cook = document.cookie.split(";").find(row=>row.startsWith(' userUrl')).split('=')[1]
     console.log("this is cookie, careful for regex : ", cookie)
     //console.log("this is cook, careful for regex : ", cook)
