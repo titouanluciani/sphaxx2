a36027d350420fc5a514f87c7a7069965ef8c768
Titouan Luciani <titouan.luciani98@gmail.com>
2021-03-10 13:35:08 +0100

    getuserurl header accesss control3

--- pages/api/getUserUrl.js
+++ pages/api/getUserUrl.js
@@ -127,9 +127,9 @@ export default async(event, context) => {
                 console.log("token updated")  
 
         }
-        /*context.setHeader('Access-Control-Allow-Origin','https://sphaxx-five.vercel.app/api/getUserUrl')
-        context.setHeader('Access-Control-Allow-Methods','POST')
-        context.setHeader('Access-Control-Allow-Headers','Content-Type')*/
+        context.setHeader('Access-Control-Allow-Origin','https://sphaxx-five.vercel.app')
+        //context.setHeader('Access-Control-Allow-Methods','POST')
+        //context.setHeader('Access-Control-Allow-Headers','Content-Type')
         context.statusCode = 200
         context.json(JSON.stringify(c))
     
