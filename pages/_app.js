604dd0b3129f156be9a8472aebf1ab65991b8860
Titouan Luciani <titouan.luciani98@gmail.com>
2021-04-20 14:28:56 +0200

    zefere

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,16 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(async () => {
     try{
-      setCookie(document.cookie.split(";").find(row=>row.startsWith('userUrl')).split('=')[1] ? document.cookie.split(";").find(row=>row.startsWith('userUrl')).split('=')[1] : document.cookie.split(";").find(row=>row.startsWith(' userUrl')).split('=')[1] )
+      if(document.cookie.split(";").find(row=>row.startsWith('userUrl')).split('=')[1]){
+        setCookie(document.cookie.split(";").find(row=>row.startsWith('userUrl')).split('=')[1])
+      }else if(document.cookie.split(";").find(row=>row.startsWith(' userUrl')).split('=')[1]){
+        setCookie(document.cookie.split(";").find(row=>row.startsWith(' userUrl')).split('=')[1])
+      }
+      if(document.cookie.split(";").find(row=>row.startsWith(' cookiesSession')).split('=')[1]){
+        setCookiesSession(document.cookie.split(";").find(row=>row.startsWith(' cookiesSession')).split('=')[1])
+      }else if(document.cookie.split(";").find(row=>row.startsWith('cookiesSession')).split('=')[1]){
+        setCookiesSession(document.cookie.split(";").find(row=>row.startsWith('cookiesSession')).split('=')[1])
+      }
       //const cook = document.cookie.split(";").find(row=>row.startsWith(' userUrl')).split('=')[1]
       setCookiesSession(document.cookie.split(";").find(row=>row.startsWith(' cookiesSession')).split('=')[1] ? document.cookie.split(";").find(row=>row.startsWith(' cookiesSession')) : document.cookie.split(";").find(row=>row.startsWith('cookiesSession') ))
     }catch(err){
