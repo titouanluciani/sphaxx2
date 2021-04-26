dee99d5ccad71e389c3a7d27b30511b76e904d81
Titouan Luciani <titouan.luciani98@gmail.com>
2021-04-26 14:30:04 +0200

    r

--- pages/api/scrapeProfiles.js
+++ pages/api/scrapeProfiles.js
@@ -89,8 +89,8 @@ export default async(event, context) => {
         await page.mouse.wheel({deltaX : x ,deltaY : y*3 })
         await delay(3000)
 
-        await page.waitForSelector('.artdeco-pagination__button.artdeco-pagination__button--next.artdeco-button.artdeco-button--muted.artdeco-button--icon-right.artdeco-button--1.artdeco-button--tertiary.ember-view')
-
+        //await page.waitForSelector('.artdeco-pagination__button.artdeco-pagination__button--next.artdeco-button.artdeco-button--muted.artdeco-button--icon-right.artdeco-button--1.artdeco-button--tertiary.ember-view')
+        
         //Get the number of pages
         const number_li = await page.$('.artdeco-pagination__indicator.artdeco-pagination__indicator--number.ember-view:last-child')
         const number_btn = await number_li.$('button')
